216fa8cd9ee499d14f97a3397cad2f2b9f1c6345
Akram-Mohammed-J <akrammohammed.j@gmail.com>
2025-02-03 23:28:47 +0530

    initial commit

---
+++ src/SinglyLinkedList/SLL.js
@@ -0,0 +1,182 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined; // If list is empty, return undefined
+
+    let current = this.head;
+    let prev = null;
+
+    while (current.next) {
+      prev = current;
+      current = current.next;
+    }
+
+    if (prev) {
+      prev.next = null;
+      this.tail = prev;
+    } else {
+      // If there was only one node
+      this.head = null;
+      this.tail = null;
+    }
+    this.length--;
+    return current; // Return the removed node
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length == 0) {
+      this.tail = null;
+    }
+    return temp;
+  }
+
+  unShift(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      // If the list is empty, set both head and tail to the new node
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // Insert the new node at the beginning
+      newNode.next = this.head;
+      this.head = newNode;
+
+      //      (or )
+      // let currHead = this.head; // Store the current head
+      // this.head = newNode; // Update head to new node
+      // newNode.next = currHead; // Link new node to previous head
+    }
+
+    this.length++; // Increase the length
+    return this;
+  }
+
+  insert(position, value) {
+    // Check if the position is out of bounds
+    if (position < 0 || position > this.length) {
+      throw new Error("Position out of bounds");
+    } else if (position === 0) {
+      // If the position is 0, insert the new node at the head
+      const newNode = new Node(value);
+      newNode.next = this.head;
+      this.head = newNode;
+      // If the list was empty, update the tail to the new node
+      if (this.length === 0) {
+        this.tail = newNode;
+      }
+      // Increment the length of the list
+      this.length++;
+    } else {
+      // Create a new node with the given value
+      const newNode = new Node(value);
+      let currNode = this.head;
+      let currPos = 0;
+      // Traverse the list to find the node before the insertion point
+      while (currPos < position - 1) {
+        currNode = currNode.next;
+        ++currPos;
+      }
+      // Insert the new node by updating the next pointers
+      newNode.next = currNode.next;
+      currNode.next = newNode;
+      // If the new node is inserted at the end, update the tail
+      if (newNode.next === null) {
+        this.tail = newNode;
+      }
+      // Increment the length of the list
+      ++this.length;
+    }
+    // Return the modified list
+    return this;
+  }
+
+  deleteAtPosition(position) {
+    // Check if the position is out of bounds
+    if (position < 0 || position >= this.length) {
+      throw new Error("Position out of bounds");
+    }
+
+    // If deleting the first node (head)
+    if (position === 0) {
+      this.head = this.head.next; // Move head to the next node
+
+      // If the list had only one node, update the tail to null
+      if (this.length === 1) {
+        this.tail = null;
+      }
+    } else {
+      // Traverse the list to find the node before the one to be deleted
+      let prev = this.head;
+      for (let i = 0; i < position - 1; i++) {
+        prev = prev.next;
+      }
+
+      // Remove the target node by updating the next pointer
+      prev.next = prev.next.next;
+
+      // If the deleted node was the last one, update the tail
+      if (position === this.length - 1) {
+        this.tail = prev;
+      }
+    }
+
+    // Decrement the length of the list after deletion
+    this.length--;
+
+    // Return the modified list
+    return this;
+  }
+}
+
+const sll = new SinglyLinkedList();
+// sll.push(30);
+// sll.push(40);
+// sll.push(50);
+
+// Insert at the beginning
+// sll.insert(0, 20);
+// console.log(JSON.stringify(sll)); // Expected: { "head": { "value": 20, "next": { "value": 30, "next": { "value": 40, "next": { "value": 50, "next": null } } } }, "tail": { "value": 50, "next": null }, "length": 4 }
+
+// // Insert in the middle
+// sll.insert(2, 35);
+// console.log(JSON.stringify(sll)); // Expected: { "head": { "value": 20, "next": { "value": 30, "next": { "value": 35, "next": { "value": 40, "next": { "value": 50, "next": null } } } } }, "tail": { "value": 50, "next": null }, "length": 5 }
+
+// // Insert at the end
+// sll.insert(5, 60);
+// console.log(JSON.stringify(sll)); // Expected: { "head": { "value": 20, "next": { "value": 30, "next": { "value": 35, "next": { "value": 40, "next": { "value": 50, "next": { "value": 60, "next": null } } } } } }, "tail": { "value": 60, "next": null }, "length": 6 }
+// console.log("removed node ", sll.pop());
+// console.log("removed node from the begining ", sll.shift());
+
+sll.unShift("Hello");
+console.log(JSON.stringify(sll));
